9a94427f8393f6545ef9ce9f17d6e40ca93c1ff8
Engenharia de Produto - Pedro Gael Feld <pedro.feld@stelladesign.local>
2025-04-08 13:29:56 -0300

    feat: inclusão de array de objetos (vídeos) para compor a funcionalidade de exibição de vídeos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const movies = [
+    {
+        img: "https://youtube.com/vi/<id>/sddefault.jpg",
+        title: "Título do vídeo",
+        link: "https://www.youtube.com/embed/<id>",
+        category: "Categoria do vídeo",
+    },
+    //...
+];
