9d95fdc175e218112d4b5f64fd9023a438673723
Pedro Feld <pedro.gael.feld@gmail.com>
2025-04-28 13:13:53 -0300

    feat: início da adição dos vídeos no array movies, inclusão dos vídeos da categoria Growcast

--- js/script.js
+++ js/script.js
@@ -1,9 +1,63 @@
 const movies = [
     {
-        img: "https://youtube.com/vi/<id>/sddefault.jpg",
-        title: "Título do vídeo",
-        link: "https://www.youtube.com/embed/<id>",
-        category: "Categoria do vídeo",
+        img: "https://youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg",
+        title: "Growcast #00 | Conheça a Growdev",
+        link: "https://www.youtube.com/embed/aJ-HZuLcKeA",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/7OWT3lfHYvE/sddefault.jpg",
+        title: "Growcast #01 | As profissões do futuro",
+        link: "https://www.youtube.com/embed/7OWT3lfHYvE",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/ci1eQGPK89o/sddefault.jpg",
+        title: "Growcast #02 | Como seguir a carreira internacional?",
+        link: "https://www.youtube.com/embed/ci1eQGPK89o",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/WM7pGG7OicQ/sddefault.jpg",
+        title: "Growcast #03 | Superando os desafios da carreira de Dev!",
+        link: "https://www.youtube.com/embed/WM7pGG7OicQ",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/2c8VOBZ160Y/sddefault.jpg",
+        title: "Growcast #04 | Agile Master: tudo sobre a profissão!",
+        link: "https://www.youtube.com/embed/2c8VOBZ160Y",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/mxnEm03cYPc/sddefault.jpg",
+        title: "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
+        link: "https://www.youtube.com/embed/mxnEm03cYPc",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/m7DyR07KrOE/sddefault.jpg",
+        title: "Growcast #06 | Mulheres e inclusão na tecnologia",
+        link: "https://www.youtube.com/embed/m7DyR07KrOE",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/vW8G9KPwRL0/sddefault.jpg",
+        title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
+        link: "https://www.youtube.com/embed/vW8G9KPwRL0",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/_Ut2KqAqPyk/sddefault.jpg",
+        title: "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
+        link: "https://www.youtube.com/embed/_Ut2KqAqPyk",
+        category: "Growcast",
+    },
+    {
+        img: "https://youtube.com/vi/dfDgs0Ive2Q/sddefault.jpg",
+        title: "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
+        link: "https://www.youtube.com/embed/dfDgs0Ive2Q",
+        category: "Growcast",
     },
     //...
 ];
